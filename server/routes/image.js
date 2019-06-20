57fefa78ecc5c4850e6d3b654860f5e924092c6f
rizkyanas35 <rizkyanas25@gmail.com>
2019-06-20 21:35:01 +0700

    upload gcs and vision done

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,9 +1,13 @@
 const routes = require("express").Router()
 const ImageController = require("../controllers/image.js")
 const {Authentication, Authorization} = require('../middlewares/auth.js')
+const TagDetection = require('../helpers/tagDetection')
+const image = require('../helpers/imageUpload')
+
 
 routes.use(Authentication)
-routes.post("/", ImageController.addImage)
+routes.post("/", image.multer.single('image'), image.sendUploadToGCS, TagDetection, ImageController.addImage)
 routes.delete("/:imageId", Authorization, ImageController.deleteImage)
+routes.post("/label-search", ImageController.labelDetection)
 
 module.exports = routes
